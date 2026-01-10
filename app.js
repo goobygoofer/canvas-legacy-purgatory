d1927c6d690f04589200854101d164e8ef45f20b
goobygoofer <joellacy8@gmail.com>
2026-01-10 11:53:51 -0600

    LOL

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ const http = require('http');
 const { Server } = require('socket.io');
 const path = require('path');
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 //Session middleware FIRST (move this above all routes)
 
 const sessionMiddleware = session({
@@ -431,6 +435,6 @@ io.on('connection', (socket) => {
 setInterval(mapUpdate, 200);
 
 const PORT = process.env.PORT || 3000;
-server.listen(3000, "0.0.0.0", () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
